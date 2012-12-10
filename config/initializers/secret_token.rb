b16159ccc4aa05d222fc917f58aa3babd2d416f7
rgarcia <voodoorai2000@gmail.com>
2012-12-11 06:08:44 +0900

    standard rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CongressApi::Application.config.secret_token = '763ab545560bddf578ee7984345bef2f7d1e7391a92f4c5c5b1cfc31f7e5e83dab2e3270674fffed9788a9bd5d787537c2ab0cc685cf079eeed6fbb3ab510d3a'
