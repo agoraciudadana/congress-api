58b23db7fdd92d1a0783227cb660ec723fdc264c
rgarcia <voodoorai2000@gmail.com>
2012-12-21 16:50:13 +0900

    allowing status to be updated

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,4 +1,4 @@
 class Proposal < ActiveRecord::Base
   attr_accessible :title, :official_url, :proposal_type, :proposed_at, 
-                  :category_name, :proposer_name, :official_resolution, :closed_at
+                  :category_name, :proposer_name, :status, :closed_at
 end
